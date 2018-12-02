26a138f591c17c1f4b28e7103d3b34660649ac7c
Phan Dung Tri <phandungtri4work@gmail.com>
2018-12-02 09:04:17 +0700

    Fix login state issue - require id to logout

--- StateMachine/LoginState.java
+++ StateMachine/LoginState.java
@@ -34,6 +34,7 @@ public class LoginState implements State<Shop>
             if(owner.getStaffList().findStaff(input)!=-1)
                 {
                     System.out.print("\nStaff "+ owner.getStaffList().getStaff(input).getFistName() +" has logged in ! \n");
+                    owner.getStateMachine().pop();
                     owner.getStateMachine().push(StaffMenuState.getInstance()); 
                 }
                
